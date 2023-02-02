4e18ca2b3f797f514ff44d80aa5f6b2c139cd432
A-Jama01 <abdurahmanjama@cmail.carleton.ca>
2023-02-02 18:20:53 -0500

    Add css styling to webpage

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     getNews();
   }, [query]);
 
-  //Fetch articles from NEWS api based on searchbar input.
+  //Fetch articles from NEWS api based on sea query state.
   const getNews = async () => {
     const response = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`);
     const data = await response.json();
@@ -24,14 +24,14 @@ function App() {
   }
 
 
-  //Searches for articles when enter is pressed. 
+  //Sets searchInput state to what is being entered in search bar
   function handleInput(e) {
     setSearchInput(e.target.value);
-    console.log(searchInput);
   }
 
+  //Sets query state to word serached when submit or enter is pressed
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault();       //prevents page from reloading after enter is pressed
     setQuery(searchInput);
     setSearchInput("");
   }
@@ -41,8 +41,8 @@ function App() {
     <>
       <Title />
       <form onSubmit={handleSubmit} className="search-bar">
-        <input type="text" placeholder="Search News" onChange={handleInput} value={searchInput} />
-        <button type="submit">Search</button>
+        <input type="text" placeholder="Search News" onChange={handleInput} value={searchInput} className="search-input" />
+        <button type="submit" className="submit">Search</button>
       </form>
       <div className="news-list">
         {newsList.map(article => (
--- src/Article.js
+++ src/Article.js
@@ -1,12 +1,12 @@
 import React from 'react';
-//import App from './App';
 
 export default function Article({ title, url, description, image }) {
   return (
     <div className='article'>
-        <h3><a href={url}>{title}</a></h3>
+        <h3>{title}</h3>
         <p>{description}</p>
         <img className='article-image' src={image} />
+        <button className='read-article'><a href={url}>Read Article</a></button>
     </div>
   )
 }
